4e7a35d4b6dcd8a096dc9b93bb19dbefc446255e
Tayfun DOGANAY <73904292+tayfundoganay@users.noreply.github.com>
2022-10-30 10:32:51 +0300

    Update and rename manavKasaProgrami to manavKasaProgrami.java

---
+++ manavKasaProgrami.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class manavKasaProgrami {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double pricePear = 2.14, priceApple = 3.67, priceTomato = 1.11, priceBanana = 0.95, priceAubergine = 5.00;
+        int kgPear, kgApple, kgTomato, kgBanana, kgAubergine;
+        System.out.print("Pear Quantity(KG) = ");
+        kgPear = scanner.nextInt();         //Entering Pear Quantity
+        System.out.print("Apple Quantity(KG) = ");
+        kgApple = scanner.nextInt();        //Entering Apple Quantity
+        System.out.print("Tomato Quantity(KG) = ");
+        kgTomato = scanner.nextInt();       //Entering Tomato Quantity
+        System.out.print("Banana Quantity(KG) = ");
+        kgBanana = scanner.nextInt();       //Entering Banana Quantity
+        System.out.print("Patlican Quantity(KG) = ");
+        kgAubergine = scanner.nextInt();    //Entering Aubergine Quantity
+
+        double cost = ((pricePear * kgPear) + (priceApple * kgApple) + (priceTomato * kgTomato) + (priceBanana * kgBanana) + (priceAubergine * kgAubergine)); //Total cost calculation and assigning data to variable
+        System.out.println("Total Cost = " + cost);
+    }
+}
