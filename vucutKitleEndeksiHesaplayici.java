120dd2f933ef929daa7b7fb4b22df07edfdeac6d
Tayfun DOGANAY <73904292+tayfundoganay@users.noreply.github.com>
2022-10-30 10:25:20 +0300

    Update and rename vucutKitleEndeksiHesaplayici to vucutKitleEndeksiHesaplayici.java

---
+++ vucutKitleEndeksiHesaplayici.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class vucutKitleEndeksiHesaplayici {
+
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+        double height, bodyMassIndex;
+        int weight;
+        System.out.println("Enter your Height and Weight.");    
+        height = scanner.nextDouble();  //Value Assignment
+        weight = scanner.nextInt(); //Value Assignment
+        bodyMassIndex = (weight / (height * height));   //Body Mass Index Calculation Result
+        System.out.println("Body Mass Index = " + bodyMassIndex);
+    }
+}
