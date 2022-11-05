f92c4f78af0ded8ff716f94636b137c507d12a37
Tayfun DOGANAY <73904292+tayfundoganay@users.noreply.github.com>
2022-11-05 10:28:07 +0300

    Update gradePassCondition.java

--- gradePassCondition.java
+++ gradePassCondition.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class gradePassCondition {
     public static void main(String[] args) {
         int math, physics,turkish, chemistry, music;
@@ -5,14 +7,19 @@ public class gradePassCondition {
 
         System.out.print("Math Grade: ");
         math = scanner.nextInt();
+        if (math <=0 || math >= 100){math = 0;}
         System.out.print("Physics Grade: ");
         physics = scanner.nextInt();
+        if (physics <=0 || physics >= 100){physics = 0;}
         System.out.print("Turkish Grade: ");
         turkish = scanner.nextInt();
+        if (turkish <=0 || turkish >= 100){turkish = 0;}
         System.out.print("Chemistry Grade: ");
         chemistry = scanner.nextInt();
+        if (chemistry <=0 || chemistry >= 100){chemistry = 0;}
         System.out.print("Music Grade: ");
         music = scanner.nextInt();
+        if (music <=0 || music >= 100){music = 0;}
 
         double average = (math + physics + turkish + chemistry + music) / 5;
 
